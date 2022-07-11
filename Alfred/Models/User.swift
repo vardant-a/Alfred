b4f498e5eface25ceb891eccb784aa89fd771057
Aleksei Lokhin <lokhin.a.a@gmail.com>
2022-07-11 21:54:00 +0300

    дополнена модель

--- Alfred/Models/User.swift
+++ Alfred/Models/User.swift
@@ -5,15 +5,24 @@
 //  Created by Алексей on 11.07.2022.
 //
 
+import QuartzCore
+
 struct User {
-    let login: String
+    var email: String = ""
     var password: String
+    var number: String = ""
     var person: Person
 }
 
 struct Person {
     var firstName: String
     var lastName: String
+    var animal: Animal
+}
+
+struct Animal {
+    let name: String
+    let kind: String
     var age: Int
-    var currentCity: String
+    
 }
